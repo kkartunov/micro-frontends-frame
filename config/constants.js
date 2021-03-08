e0522e6c7e2bc62166a51c855e810df9e8b0d866
Kiril Kartunov <kiril.kartunov@gmail.com>
2021-03-08 11:40:09 +0200

    heroku server

--- config/constants.js
+++ config/constants.js
@@ -1,39 +1,36 @@
 module.exports = Object.freeze({
   APP_CONFIG: [
     {
-      'appEnv': 'dev',
-      'mfeConfigPath': 'https://tc-platform-dev.s3.amazonaws.com/micro-frontends/micro-frontends-config-development.json',
-      'mfeIndexPath': '/index.html',
-      'mfeRoutesPath': 'https://tc-platform-dev.s3.amazonaws.com/micro-frontends/micro-frontends-routes-development.txt'
+      appEnv: "dev",
+      mfeConfigPath:
+        "https://tc-platform-dev.s3.amazonaws.com/micro-frontends/micro-frontends-config-development.json",
+      mfeIndexPath: "/index.html",
+      mfeRoutesPath:
+        "https://tc-platform-dev.s3.amazonaws.com/micro-frontends/micro-frontends-routes-development.txt",
     },
     {
-      'appEnv': 'prod',
-      'mfeConfigPath': 'https://tc-platform-prod.s3.amazonaws.com/micro-frontends/micro-frontends-config-production.json',
-      'mfeIndexPath': '/index.html',
-      'mfeRoutesPath': 'https://tc-platform-prod.s3.amazonaws.com/micro-frontends/micro-frontends-routes-production.txt'
+      appEnv: "prod",
+      mfeConfigPath:
+        "https://tc-platform-prod.s3.amazonaws.com/micro-frontends/micro-frontends-config-production.json",
+      mfeIndexPath: "/index.html",
+      mfeRoutesPath:
+        "https://tc-platform-prod.s3.amazonaws.com/micro-frontends/micro-frontends-routes-production.txt",
     },
     {
-      'appEnv': 'local-multi',
-      'mfeConfigPath': '/micro-frontends-config-local.json',
-      'mfeIndexPath': 'http://localhost:8081/index.html',
-      'mfeRoutesPath': '/micro-frontends-routes-local.txt'
+      appEnv: "local-multi",
+      mfeConfigPath: "/micro-frontends-config-local.json",
+      mfeIndexPath: "http://localhost:8081/index.html",
+      mfeRoutesPath: "/micro-frontends-routes-local.txt",
     },
     {
-      'appEnv': 'local',
-      'mfeConfigPath': '/micro-frontends-config-local.json',
-      'mfeIndexPath': '/index.html',
-      'mfeRoutesPath': '/micro-frontends-routes-local.txt'
-    },
-    {
-      'appEnv': 'heroku',
-      'mfeConfigPath': '/micro-frontends-config-heroku.json',
-      'mfeIndexPath': 'http://localhost:8081/index.html',
-      'mfeRoutesPath': '/micro-frontends-routes-local.txt'
+      appEnv: "local",
+      mfeConfigPath: "/micro-frontends-config-local.json",
+      mfeIndexPath: "/index.html",
+      mfeRoutesPath: "/micro-frontends-routes-local.txt",
     },
   ],
   APP_ENV_DEV: "dev",
   APP_ENV_PROD: "prod",
   APP_ENV_LOCAL_MULTI: "local-multi",
   APP_ENV_LOCAL: "local",
-  APP_ENV_HEROKU: "heroku",
 });
--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 
 const express = require("express");
 const path = require("path");
-const axios = require('axios');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
-const htmlParse = require('node-html-parser');
-const APP_CONST = require('./config/constants');
+const axios = require("axios");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const htmlParse = require("node-html-parser");
+const APP_CONST = require("./config/constants");
 
 let distPath = path.resolve(__dirname, "./dist");
 let configPath = path.resolve(__dirname, "./config");
@@ -16,76 +16,91 @@ const app = express();
 app.use(express.static(distPath));
 
 app.get("/micro-frontends-config", async function (req, res) {
-  let mfeRoutes
-  var env_config = APP_CONST.APP_CONFIG.find(function (item) { return item.appEnv == process.env.APPENV.toLowerCase() })
+  let mfeRoutes;
+  var env_config = APP_CONST.APP_CONFIG.find(function (item) {
+    return item.appEnv == process.env.APPENV.toLowerCase();
+  });
   switch (process.env.APPENV.toLowerCase()) {
-    case APP_CONST.APP_ENV_PROD.toLowerCase() :
-    case APP_CONST.APP_ENV_DEV.toLowerCase() :
-      mfeRoutes = await axios.get(env_config.mfeConfigPath)
-      mfeRoutes = mfeRoutes.data
+    case APP_CONST.APP_ENV_PROD.toLowerCase():
+    case APP_CONST.APP_ENV_DEV.toLowerCase():
+      mfeRoutes = await axios.get(env_config.mfeConfigPath);
+      mfeRoutes = mfeRoutes.data;
       break;
-    case APP_CONST.APP_ENV_LOCAL.toLowerCase() :
-    case APP_CONST.APP_ENV_LOCAL_MULTI.toLowerCase() :
-    case APP_CONST.APP_ENV_HEROKU.toLowerCase() :
-      mfeRoutes = await fsPromises.readFile(path.join(configPath + env_config.mfeConfigPath))
+    case APP_CONST.APP_ENV_LOCAL.toLowerCase():
+    case APP_CONST.APP_ENV_LOCAL_MULTI.toLowerCase():
+      mfeRoutes = await fsPromises.readFile(
+        path.join(configPath + env_config.mfeConfigPath)
+      );
       break;
-    default :
-      res.send({'error': { message: "Check application environment", code: 500 }})
+    default:
+      res.send({
+        error: { message: "Check application environment", code: 500 },
+      });
       break;
   }
-  console.log("/micro-frontends-config", env_config, process.env.APPENV.toLowerCase(), mfeRoutes)
   res.send(mfeRoutes);
 });
 
 app.get("*", async function (req, res) {
-  var env_config = APP_CONST.APP_CONFIG.find(function (item) { return item.appEnv == process.env.APPENV.toLowerCase() })
-  if (!await fs.existsSync(distPath)){
+  var env_config = APP_CONST.APP_CONFIG.find(function (item) {
+    return item.appEnv == process.env.APPENV.toLowerCase();
+  });
+  if (!(await fs.existsSync(distPath))) {
     await fs.mkdirSync(distPath);
   }
-  let mfeIndex
+  let mfeIndex;
   switch (process.env.APPENV.toLowerCase()) {
-    case APP_CONST.APP_ENV_PROD.toLowerCase() :
-    case APP_CONST.APP_ENV_DEV.toLowerCase() :
-    case APP_CONST.APP_ENV_LOCAL.toLowerCase() :
-      mfeIndex = await fsPromises.readFile(path.join(distPath + env_config.mfeIndexPath))
+    case APP_CONST.APP_ENV_PROD.toLowerCase():
+    case APP_CONST.APP_ENV_DEV.toLowerCase():
+    case APP_CONST.APP_ENV_LOCAL.toLowerCase():
+      mfeIndex = await fsPromises.readFile(
+        path.join(distPath + env_config.mfeIndexPath)
+      );
       break;
-    case APP_CONST.APP_ENV_LOCAL_MULTI.toLowerCase() :
-      mfeIndex = await axios.get(env_config.mfeIndexPath)
-      mfeIndex = mfeIndex.data
+    case APP_CONST.APP_ENV_LOCAL_MULTI.toLowerCase():
+      mfeIndex = await axios.get(env_config.mfeIndexPath);
+      mfeIndex = mfeIndex.data;
       break;
-    case APP_CONST.APP_ENV_HEROKU.toLowerCase() :
-      mfeIndex = await axios.get(env_config.mfeIndexPath)
-      mfeIndex = mfeIndex.data
-      break;
-    default :
-      res.send({'error': { message: "Check application environment", code: 500 }})
+    default:
+      res.send({
+        error: { message: "Check application environment", code: 500 },
+      });
       break;
   }
-  let mfeRoutes
+  let mfeRoutes;
   switch (process.env.APPENV.toLowerCase()) {
-    case APP_CONST.APP_ENV_DEV.toLowerCase() :
-    case APP_CONST.APP_ENV_PROD.toLowerCase() :
-      mfeRoutes = await axios.get(env_config.mfeRoutesPath)
-      mfeRoutes = mfeRoutes.data
+    case APP_CONST.APP_ENV_DEV.toLowerCase():
+    case APP_CONST.APP_ENV_PROD.toLowerCase():
+      mfeRoutes = await axios.get(env_config.mfeRoutesPath);
+      mfeRoutes = mfeRoutes.data;
       break;
-    case APP_CONST.APP_ENV_LOCAL.toLowerCase() :
-    case APP_CONST.APP_ENV_LOCAL_MULTI.toLowerCase() :
-    case APP_CONST.APP_ENV_HEROKU.toLowerCase() :
-      mfeRoutes = await fsPromises.readFile(path.join(configPath + env_config.mfeRoutesPath))
+    case APP_CONST.APP_ENV_LOCAL.toLowerCase():
+    case APP_CONST.APP_ENV_LOCAL_MULTI.toLowerCase():
+      mfeRoutes = await fsPromises.readFile(
+        path.join(configPath + env_config.mfeRoutesPath)
+      );
       break;
-    default :
-      res.send({'error': { message: "Check application environment", code: 500 }})
+    default:
+      res.send({
+        error: { message: "Check application environment", code: 500 },
+      });
       break;
   }
-  let mfeIndexHtml = htmlParse.parse(mfeIndex)
-  let singleSpaMain = mfeIndexHtml.querySelector('#single-spa-main')
-  singleSpaMain.remove()
-  let singleSpaRouter = mfeIndexHtml.querySelector('single-spa-router')
-  singleSpaRouter.insertAdjacentHTML('beforeend', '<div id="single-spa-main"></div>')
-  singleSpaMain = mfeIndexHtml.querySelector('#single-spa-main')
-  singleSpaMain.insertAdjacentHTML('beforeend', mfeRoutes)
+  let mfeIndexHtml = htmlParse.parse(mfeIndex);
+  let singleSpaMain = mfeIndexHtml.querySelector("#single-spa-main");
+  singleSpaMain.remove();
+  let singleSpaRouter = mfeIndexHtml.querySelector("single-spa-router");
+  singleSpaRouter.insertAdjacentHTML(
+    "beforeend",
+    '<div id="single-spa-main"></div>'
+  );
+  singleSpaMain = mfeIndexHtml.querySelector("#single-spa-main");
+  singleSpaMain.insertAdjacentHTML("beforeend", mfeRoutes);
 
-  await fsPromises.writeFile(path.join(distPath + "/index.html"), mfeIndexHtml.toString());
+  await fsPromises.writeFile(
+    path.join(distPath + "/index.html"),
+    mfeIndexHtml.toString()
+  );
   res.sendFile(path.join(distPath + "/index.html"));
 });
 
